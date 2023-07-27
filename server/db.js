e0e1f5342169a0c56fff2e9cb5dc3720a9b48348
Adesh <sawantadesh2003@gmail.com>
2023-07-27 19:42:04 +0530

    Inital commit

---
+++ server/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.mongoURI;
+
+// console.log(process.env.mongoURI)
+const connetToDB = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to the database");
+    }
+    catch (err) {
+        throw err;
+    }
+}
+module.exports = connetToDB;
