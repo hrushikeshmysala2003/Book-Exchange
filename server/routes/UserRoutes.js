405da13578b1d2474b16e3644c4d4183560b3ecd
rushi <hrushikeshmysala2003@gmail.com>
2023-07-29 18:13:09 +0530

    Added login and logout routes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, demoUser } = require("../controllers/UserController");
+const { registerUser, loginUser, demoUser, logoutUser } = require("../controllers/UserController");
 const singleUpload = require("../middlewares/multer");
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route("/demo").get(demoUser);
 router.route("/register").post( singleUpload ,registerUser);
 router.route("/login").post(loginUser);
+router.route("/logout").get(logoutUser);
 
 module.exports = router;
