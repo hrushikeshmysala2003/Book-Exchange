f08e45a04b5ad22e56675146f984722e9987a679
rushi <hrushikeshmysala2003@gmail.com>
2023-08-02 23:07:46 +0530

    Added change password page

--- book-exchanger/src/screens/AuthPages/ChangePassword.jsx
+++ book-exchanger/src/screens/AuthPages/ChangePassword.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const ChangePassword = () => {
   return (
-    <div>
-      ChangePassword
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 to-blue-500"  >
+        <div className="w-96 p-3 bg-white rounded-lg m-3" >
+            <h1 className="text-3xl block text-center font-semibold " >Change Password</h1>
+            <hr className="mt-2 text-lg" />
+            <div className="my-3 " >
+                <label htmlFor="email" className="my-3 block text-base mb-2" >Old Password : </label>
+                <input type="password" id='email' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Email" />
+            </div>
+            <div className="mt-3" >
+                <label htmlFor="password" className="my-3 block text-base mb-2" >New Password : </label>
+                <input type="password" id='password' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter Password" />
+            </div>
+            
+
+            <div>
+                <button className="border-2 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500  text-white mt-3 p-1.5 rounded-md w-full" >Change Password</button>
+            </div>
+           
+            
+        </div>
     </div>
   )
 }
