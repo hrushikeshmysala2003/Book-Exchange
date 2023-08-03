5e275b1ef28b1c06b6fef50225e15d381f5b945b
Adesh <sawantadesh2003@gmail.com>
2023-08-03 14:57:10 +0530

    updated auth pages

--- book-exchanger/src/screens/AuthPages/Login.jsx
+++ book-exchanger/src/screens/AuthPages/Login.jsx
@@ -3,20 +3,20 @@ import {Link} from "react-router-dom"
 const Login = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 to-blue-500"  >
-        <div className="w-96 p-3 bg-white rounded-lg m-3" >
+        <form className="w-96 p-4 bg-white rounded-lg m-3" >
             <h1 className="text-3xl block text-center font-semibold " > <i className="fa-solid fa-user pr-2"></i> Login</h1>
             <hr className="mt-2 text-lg" />
             <div className="my-3 " >
                 <label htmlFor="email" className="my-3 block text-base mb-2" >Email : </label>
-                <input type="email" id='email' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Email" />
+                <input type="email" id='email' className=" rounded-md border w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Email" />
             </div>
             <div className="mt-3" >
                 <label htmlFor="password" className="my-3 block text-base mb-2" >Password : </label>
-                <input type="password" id='password' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter Password" />
+                <input type="password" id='password' className=" rounded-md border w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter Password" />
             </div>
             <div className="my-4 flex justify-end items-center" >
                 <div>
-                    <input id="remember" type="checkbox" />
+                    <input id="remember" type="checkbox" className='mr-1' />
                     <label  htmlFor="">Remember Me</label>
                 </div>
             </div>
@@ -32,7 +32,7 @@ const Login = () => {
                     <Link to={"/forgetpassword"} className=" text-sm italic text-indigo-800 " >Forgot Password?</Link>
             </div>
             
-        </div>
+        </form>
     </div>
   )
 }
--- book-exchanger/src/screens/AuthPages/Register.jsx
+++ book-exchanger/src/screens/AuthPages/Register.jsx
@@ -3,32 +3,31 @@ import { Link } from "react-router-dom"
 
 import "./fileUploadCss.css"
 
-
 const Register = () => {
   
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-400 to-blue-500"  >
-        <div className="w-96 p-3 bg-white rounded-lg m-3" >
+        <form className="w-96 p-4 bg-white rounded-lg m-3" >
             <h1 className="text-3xl block text-center font-semibold " > <i className="fa-solid fa-user pr-2"></i> Register</h1>
             <hr className="mt-2 text-lg" />
             <div className="w-20 my-3 h-20 flex justify-center mx-auto" >
-              <img src="https://res.cloudinary.com/dwna7axtx/image/upload/v1690702045/ws0sv4eb57qlyakpt8nc.png" alt="" />
+              <img src="https://res.cloudinary.com/dwna7axtx/image/upload/v1690702045/ws0sv4eb57qlyakpt8nc.png" alt="avatar" />
             </div>
             <div className="my-3 " >
                 <label htmlFor="name" className="my-3 block text-base mb-2" >Name : </label>
-                <input type="text" id='name' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Name" />
+                <input type="text" id='name' className=" rounded-md border border-gray-200 order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Name" />
             </div>
             <div className="my-3 " >
                 <label htmlFor="email" className="my-3 block text-base mb-2" >Email : </label>
-                <input type="email" id='email' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Email" />
+                <input type="email" id='email' className=" rounded-md border border-gray-200 order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter your Email" />
             </div>
             <div className="mt-3" >
                 <label htmlFor="password" className="my-3 block text-base mb-2" >Password : </label>
-                <input type="password" id='password' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter Password" />
+                <input type="password" id='password' className=" rounded-md border border-gray-200 order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" placeholder="Enter Password" />
             </div>
             <div className="mt-3" >
                 <label htmlFor="file" className="my-3 block text-base mb-2" >Profile picture : </label>
-                <input  type="file" id='file' className=" rounded-md order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" />
+                <input  type="file" id='file' className=" rounded-md border border-gray-200 order w-full text-base px-2 py-1 focus:outline-none focus:ring focus:border-blue-600" />
             </div>
 
             <div>
@@ -42,7 +41,7 @@ const Register = () => {
                     
             </div>
             
-        </div>
+        </form>
     </div>
   )
 }
