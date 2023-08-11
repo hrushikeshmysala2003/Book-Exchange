ed1f08a851ff6107194425d990c58f698e371b42
rushi <hrushikeshmysala2003@gmail.com>
2023-08-11 13:06:53 +0530

    Added Profile Page

--- book-exchanger/src/redux/store.js
+++ book-exchanger/src/redux/store.js
@@ -7,7 +7,8 @@ export const store = configureStore({
     reducer: {
         user: userReducer,
         profile: profileReducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 })
 
 export const server = "http://localhost:5000/api/v1"
