c21763f682be1cba2ea2ec775d8cc38bec41a89d
rushi <hrushikeshmysala2003@gmail.com>
2024-07-29 14:07:52 +0530

    Added nice responsive UI for chat page

--- book-exchanger/src/screens/Chat/Chat.jsx
+++ book-exchanger/src/screens/Chat/Chat.jsx
@@ -20,14 +20,57 @@ const Chat = () => {
     setMessage("");
   };
 
-  // setTimeout(() => {
-  //   setUserId(user._id);
-  //   setUserName(user.name);
-  // }, 5000);
+  const messages = [
+    {
+      senderId: "64fb0432c35277d057f7640a",
+      senderName: "Rahul",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni corporis totam tempore voluptas iusto distinctio corrupti sed incidunt accusantium!",
+      _id: "66a70ebc5ca04b4f47eb6c04",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640e",
+      senderName: "Hrushikesh",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. corrupti sed incidunt accusantium!",
+      _id: "66a70ee50d2c37d78df11306",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640a",
+      senderName: "Rahul",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni corporis totam tempore voluptas iusto distinctio corrupti sed incidunt accusantium!",
+      _id: "66a70ebc5ca04b4f47eb6c04",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640e",
+      senderName: "Hrushikesh",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. corrupti sed incidunt accusantium!",
+      _id: "66a70ee50d2c37d78df11306",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640a",
+      senderName: "Rahul",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni corporis totam tempore voluptas iusto distinctio corrupti sed incidunt accusantium!",
+      _id: "66a70ebc5ca04b4f47eb6c04",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640e",
+      senderName: "Hrushikesh",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. corrupti sed incidunt accusantium!",
+      _id: "66a70ee50d2c37d78df11306",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640a",
+      senderName: "Rahul",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit magni corporis totam tempore voluptas iusto distinctio corrupti sed incidunt accusantium!",
+      _id: "66a70ebc5ca04b4f47eb6c04",
+    },
+    {
+      senderId: "64fb0432c35277d057f7640e",
+      senderName: "Hrushikesh",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. corrupti sed incidunt accusantium!",
+      _id: "66a70ee50d2c37d78df11306",
+    },
+  ];
 
-  // console.log(user);
-  // setUserName(user.name);
-  // setUserId(user._id);
   useEffect(() => {
     socket.on("connect", () => {
       console.log("Connected", socket.id);
@@ -44,22 +87,49 @@ const Chat = () => {
   }, []);
 
   return (
-    <div>
-      <form>
+    <div className="h-screen w-screen flex items-center justify-center flex-col">
+      <div className="h-2/3 overflow-y-scroll flex flex-col gap-y-2 w-full py-3 sm:w-1/2 md:w-2/3 lg:w-1/3">
+        {user &&
+          messages.map((message) => {
+            if (message.senderId === user._id) {
+              return (
+                <div key={message._id} className="flex gap-x-2 justify-end ">
+                  <p className="text-sm text-zinc-500 ">{"me"}</p>
+                  <p className="bg-blue-200 p-2 rounded-lg w-4/5  ">
+                    {message.text}
+                  </p>{" "}
+                </div>
+              );
+            } else {
+              return (
+                <div key={message._id} className="flex gap-x-2">
+                  <p className="bg-blue-300 p-2 rounded-lg w-4/5 ">
+                    {message.text}
+                  </p>{" "}
+                  <p className="text-sm text-zinc-500 ">{message.senderName}</p>
+                </div>
+              );
+            }
+          })}
+      </div>
+      <form className=" rounded-md flex justify-between border border-slate-500 w-full sm:w-1/2 md:w-2/3 lg:w-1/3">
         <input
+          className="rounded-md p-2 m-1  w-full "
           type="text"
           value={message}
           onKeyUp={(e) => (e.key === "Enter" ? messageSendHandler(e) : null)}
           onChange={(e) => setMessage(e.target.value)}
+          placeholder="Enter the message"
         />
         <br />
-        <button onClick={(e) => messageSendHandler(e)} type="submit">
+        <button
+          className="p-3 bg-blue-400 rounded-lg m-1 "
+          onClick={(e) => messageSendHandler(e)}
+          type="submit"
+        >
           Submit
         </button>
       </form>
-      <p>
-        {userId} {userName}
-      </p>
     </div>
   );
 };
