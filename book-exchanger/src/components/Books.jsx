7cae346cf71b1d2bf0e9dcd603deb7d53ebc4e26
rushi <hrushikeshmysala2003@gmail.com>
2024-10-10 15:04:29 +0530

    Final Commit

--- book-exchanger/src/components/Books.jsx
+++ book-exchanger/src/components/Books.jsx
@@ -1,54 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import Card from './Card'
-import axios from 'axios'
-import { server } from '../redux/store'
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import { server } from "../redux/store";
 
 function Books() {
+  // const book = {
+  //     name: "Importance of Book Exchange",
+  //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, asperiores blanditiis dignissimos enim minima delectus, commodi repellendus perspiciatis libero, modi voluptatibus sapiente praesentium beatae repellat! Voluptates rerum animi doloremque laudantium.",
+  //     img:{
+  //         url:'https://bookxchange.in/assets/img/blog/blog1.jpg'
+  //     },
+  //     price: 100,
+  // }
+  const [data, setData] = useState([]);
 
-    // const book = {
-    //     name: "Importance of Book Exchange",
-    //     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, asperiores blanditiis dignissimos enim minima delectus, commodi repellendus perspiciatis libero, modi voluptatibus sapiente praesentium beatae repellat! Voluptates rerum animi doloremque laudantium.",
-    //     img:{
-    //         url:'https://bookxchange.in/assets/img/blog/blog1.jpg'
-    //     },
-    //     price: 100,
-    // }
-    const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(`${server}/getallbooks`);
 
-    useEffect(() => {
-        async function getData() {
-            const res = await axios.get(`${server}/getallbooks`);
+      setData(res.data.books);
+    }
 
-            setData(res.data.books);
-        }
+    getData();
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <div>
+        <div className="text-center text-[36px] text-[#363A45] py-[2rem]">
+          <h2>
+            Best Selling{" "}
+            <span class="bg-blue-100 text-blue-800 text-[36px] font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+              Books
+            </span>
+          </h2>
+        </div>
 
-        getData();
-    }, [])
-    console.log(data);
-    return (
-        <>
-            <div>
-                <div className='text-center text-[36px] text-[#363A45] py-[2rem]'>
-                    <h2>Best Selling <span class="bg-blue-100 text-blue-800 text-[36px] font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Books</span></h2>
-                </div>
-
-                <div className='bg-[#F5F5F5] w-full flex justify-center p-[3rem]'>
-                    <div className='flex flex-wrap justify-center w-[90%] gap-[2rem]'>
-                        {/* <Card book={book} />
+        <div className="bg-[#F5F5F5] w-full flex justify-center p-[3rem]">
+          <div className="flex flex-wrap justify-center w-[90%] gap-[2rem]">
+            {/* <Card book={book} />
                         <Card book={book} />
                         <Card book={book} /> */}
-                        {
-                            data.map((book)=>{
-                                const book_details={name:book.title,img:book.image,description:book.description,price:book.price}
+            {data.map((book) => {
+              const book_details = {
+                name: book.title,
+                img: book.image,
+                description: book.description,
+                price: book.price,
+                _id: book._id,
+              };
 
-                                return <Card book={book_details} />
-                            })
-                        }
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+              return <Card book={book_details} />;
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Books
+export default Books;
--- book-exchanger/src/components/Card.jsx
+++ book-exchanger/src/components/Card.jsx
@@ -1,34 +1,55 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Card({ book }) {
-    // console.log(book);
-    const [desc, setdesc] = useState(book.description.substring(0, Math.min(100, book.description.length)))
+  // console.log(book);
+  const [desc, setdesc] = useState(
+    book.description.substring(0, Math.min(100, book.description.length))
+  );
 
-    function handleDesc() {
-        if (desc.length === 100)
-            setdesc(book.description);
-        else
-            setdesc(book.description.substring(0, 100))
-    }
-    return (
-        <div className='text-black w-[300px] flex flex-col gap-2 border border-gray-100 rounded shadow-lg bg-white'>
-            <div>
-                <img src={book.img.url} alt="img" className='rounded h-[150px] w-[300px] object-cover' />
-            </div>
-            <div className='p-3 text-center flex-grow'>
-                <h3 className='font-bold text-xl mb-2'>{book.name}</h3>
-                {
-                    desc.length >= 100 ?
-                        <p className='text-gray-500'>{desc + '...'}{
-                            desc.length !== book.description.length ? <button className='text-blue-400' onClick={handleDesc}>Read more</button> : <button className='text-blue-400' onClick={handleDesc}>Show less</button>
-                        } </p> : <p className='text-gray-500'>{desc}</p>
-                }
-            </div>
-            <div className='border-t border-t-gray-200 text-center py-2 mx-4'>
-                <span className="text-gray-700 text-2xl font-black">${book.price}</span>
-            </div>
-        </div>
-    )
+  function handleDesc() {
+    if (desc.length === 100) setdesc(book.description);
+    else setdesc(book.description.substring(0, 100));
+  }
+  return (
+    <div className="text-black w-[300px] flex flex-col gap-2 border border-gray-100 rounded shadow-lg bg-white">
+      <div>
+        <img
+          src={book.img.url}
+          alt="img"
+          className="rounded h-[150px] w-[300px] object-cover"
+        />
+      </div>
+      <div className="p-3 text-center flex-grow">
+        <h3 className="font-bold text-xl mb-2">{book.name}</h3>
+        {desc.length >= 100 ? (
+          <p className="text-gray-500">
+            {desc + "..."}
+            {desc.length !== book.description.length ? (
+              <button className="text-blue-400" onClick={handleDesc}>
+                Read more
+              </button>
+            ) : (
+              <button className="text-blue-400" onClick={handleDesc}>
+                Show less
+              </button>
+            )}{" "}
+          </p>
+        ) : (
+          <p className="text-gray-500">{desc}</p>
+        )}
+      </div>
+      <div className="border-t border-t-gray-200 text-center py-2 mx-4">
+        <span className="text-gray-700 text-2xl font-black">${book.price}</span>
+      </div>
+      <div className="border-t border-t-gray-200 text-center py-2 mx-4">
+        <Link to={`/chat/${book._id}`}>
+          <button className="rounded-lg w-full bg-blue-400 hover:bg-blue-700">
+            Join chat
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
